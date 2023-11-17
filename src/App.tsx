e5727a2d2180ff58734ac5115ec9aff5ef0faa09
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-17 12:23:16 +0100

    asdsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useContext } from 'react'
+// import { useContext } from 'react'
 import './App.css'
-import WelcomePage from './Pages/WelcomePage/WelcomePage'
-import { AuthContext } from './Contexts/UserContext'
+// import WelcomePage from './Pages/WelcomePage/WelcomePage'
+// import { AuthContext } from './Contexts/UserContext'
 import HomePage from './Pages/HomePage/HomePage'
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 
 function App() {
-  const { user } = useContext(AuthContext)
+  // const { user } = useContext(AuthContext)
 
   return (
     <div>
